433f8d18575412b9701dffa19e745f344a25598b
MejiaDeRul <165450391+MejiaDeRul@users.noreply.github.com>
2024-05-26 19:26:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import requests
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'supersecretkey'
-app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY') or 'superjwtsecretkey'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 
 jwt = JWTManager(app)
 

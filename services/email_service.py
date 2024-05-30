727df5352e9dc1298073a4c7b00e720b9fd8f24f
Juan Mejia <juanmejia234255@gmail.com>
2024-05-29 19:56:35 -0500

    cambios en email

--- services/email_service.py
+++ services/email_service.py
@@ -1,7 +1,8 @@
 from flask_mail import Message, Mail
-from flask import url_for
+from flask import url_for, current_app
 
 mail = Mail(__name__)
+mail.init_app(current_app)
 
 # Funcion para enviar url para verificar correo
 def send_verification_email(correo, usuario, codigo_verificacion):
